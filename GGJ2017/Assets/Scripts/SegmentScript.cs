460ec126f00419cf0da49aaf099fb466db08f32a
Aggasti <oliver.lazarevski@gmail.com>
2017-01-21 01:23:11 +0100

    full checkin

---
+++ GGJ2017/Assets/Scripts/SegmentScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class SegmentScript : MonoBehaviour
+{
+    public float speedOfTravel;
+
+    private Vector3 _direction;
+
+    void Update()
+    {
+        transform.Translate(_direction * Time.deltaTime * speedOfTravel);
+    }
+
+    public void SetDirection(float x, float y)
+    {
+        _direction = new Vector3(x, y);
+    }
+}
---
+++ GGJ2017/Assets/Scripts/SpawnOnClick.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class SpawnOnClick : MonoBehaviour {
+
+    public GameObject spawnerSample;
+
+    void Update () {		
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {            
+            Vector2 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            GameObject spawner = Instantiate(spawnerSample);
+            spawner.transform.position = position;
+        }
+	}
+}
