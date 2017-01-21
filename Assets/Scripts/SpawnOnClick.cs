af471428dfeb4a5d53217952931a9cf00ed751ea
Aggasti <oliver.lazarevski@gmail.com>
2017-01-21 10:01:13 +0100

    Added Spawn Area

--- Assets/Scripts/SpawnOnClick.cs
+++ Assets/Scripts/SpawnOnClick.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
 
-public class SpawnOnClick : MonoBehaviour {
+public class SpawnOnClick : MonoBehaviour
+{
 
     public GameObject spawnerSample;
 
-    void Update () {		
-        if(Input.GetKeyDown(KeyCode.Mouse0))
-        {            
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
             Vector2 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            GameObject spawner = Instantiate(spawnerSample);
-            spawner.transform.position = position;
+            RaycastHit2D hit = Physics2D.Raycast(position, Vector2.zero);
+            if (hit.collider != null && hit.collider.tag == "SpawnArea")
+            {
+                GameObject spawner = Instantiate(spawnerSample);
+                spawner.transform.position = position;
+            }
+
         }
-	}
+    }
 }
