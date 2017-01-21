3aecb93f936cb125a04cdd28aa11577c9d81f463
Aggasti <oliver.lazarevski@gmail.com>
2017-01-21 20:37:49 +0100

    lose

--- Assets/Scripts/SpawnOnClick.cs
+++ Assets/Scripts/SpawnOnClick.cs
@@ -2,20 +2,34 @@
 
 public class SpawnOnClick : MonoBehaviour
 {
-
     public GameObject spawnerSample;
+    public GameObject loseCanvas;
 
+    private int _loseCounter = 2;
+    private Rigidbody2D _loptaRigidBody2D;
+
+    private void Start()
+    {
+        _loptaRigidBody2D = GameObject.FindObjectOfType<LoptaScript>().GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0) && FindObjectsOfType<ParticleSpawnerScript>().Length == 0)
-        {          
+        if (_loseCounter > 0 && Input.GetKeyDown(KeyCode.Mouse0) && FindObjectsOfType<ParticleSpawnerScript>().Length == 0)
+        {
             Vector2 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(position, Vector2.zero);
             if (hit.collider != null && hit.collider.tag == "SpawnArea")
             {
                 GameObject spawner = Instantiate(spawnerSample);
                 spawner.transform.position = position;
+                _loseCounter--;
             }
+        }
+
+        if (_loseCounter == 0 && _loptaRigidBody2D != null && _loptaRigidBody2D.velocity == Vector2.zero && FindObjectsOfType<ParticleSpawnerScript>().Length == 0)
+        {
+            _loseCounter = -1;
+            loseCanvas.SetActive(true);
         }
     }
 }
