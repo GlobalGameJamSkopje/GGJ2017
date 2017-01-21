425e43329697fa74ea91473f2bd9340d40558d75
Aggasti <oliver.lazarevski@gmail.com>
2017-01-21 02:34:15 +0100

    CollisionDestroyer

---
+++ Assets/Scripts/ParticleSpawnerScript.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+public class ParticleSpawnerScript : MonoBehaviour
+{
+    public int segments;
+    public float radius;
+
+    
+
+
+    public SegmentScript sample;
+
+    private SegmentScript[] particles;
+    void Start()
+    {
+        particles = new SegmentScript[segments];
+
+        float angle = 20f;
+        for (int i = 0; i < segments; i++)
+        {
+            var x = Mathf.Sin(Mathf.Deg2Rad * angle) * radius;
+            var y = Mathf.Cos(Mathf.Deg2Rad * angle) * radius;
+
+            particles[i] = CreateParticle(x, y);
+
+            if (i > 0 && i < segments)
+                Connect(particles[i], particles[i - 1]);
+            if (i == segments - 1)
+                Connect(particles[0], particles[i]);
+
+            angle += (360f / segments);
+            particles[i].GetComponent<SegmentScript>().SetDirection(x, y);
+        }
+    }
+
+    void Update()
+    {
+        UpdateConnectingLines();
+    }
+
+    private SegmentScript CreateParticle(float x, float y)
+    {
+        SegmentScript particle = Instantiate(sample, gameObject.transform) as SegmentScript;
+        particle.transform.position = new Vector2(x + transform.position.x, y + transform.position.y);
+        return particle;
+    }
+
+    void Connect(SegmentScript particle1, SegmentScript particle2)
+    {
+        if (particle1 == null || particle2 == null)
+            return;
+
+        LineRenderer line = particle1.GetComponent<LineRenderer>();
+        line.sortingOrder = 2;
+        line.numPositions = 2;
+        line.SetPosition(0, particle1.transform.position);
+        line.SetPosition(1, particle2.transform.position);
+    }
+
+    void UpdateConnectingLines()
+    {
+        for (int i = 0; i < segments; i++)
+        {
+            if (i > 0 && i < segments)
+            {
+                Connect(particles[i], particles[i - 1]);
+
+                if (particles[i] == null
+                    && i < segments - 1)
+                    DisableLineRenderer(particles[i + 1]);
+            }
+            if (i == segments - 1)
+            {
+                Connect(particles[0], particles[i]);
+
+                if (particles[i] == null)
+                    DisableLineRenderer(particles[0]);
+            }
+        }
+    }
+    void DisableLineRenderer(SegmentScript particle)
+    {
+        if (particle == null)
+            return;
+
+        particle.GetComponent<LineRenderer>().enabled = false;
+    }
+}
---
+++ Assets/Scripts/SegmentScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class SegmentScript : MonoBehaviour
+{
+    public float speedOfTravel;
+
+    private Vector3 _direction;
+
+    private float _timeAlive = 0;
+    
+
+    // commented line 
+    public GameObject SpawnerSegment;
+    // end
+
+    void Update()
+    {
+        _timeAlive += Time.deltaTime;
+        transform.Translate(_direction * Time.deltaTime * speedOfTravel);
+    }
+
+    public void SetDirection(float x, float y)
+    {
+        _direction = new Vector3(x, y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(_timeAlive > 0.2f)
+        {
+            GameObject spawnerSegment = Instantiate(SpawnerSegment);
+            spawnerSegment.transform.position = gameObject.transform.position;
+        }     
+
+        if (collision.gameObject.tag == "Destroyer")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
