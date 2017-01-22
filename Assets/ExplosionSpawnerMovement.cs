3ee23f3bc26f9b76b45d45ccd5881d122fafedd3
Aggasti <oliver.lazarevski@gmail.com>
2017-01-22 15:37:28 +0100

    10 levels completed

---
+++ Assets/ExplosionSpawnerMovement.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class ExplosionSpawnerMovement : MonoBehaviour {
+
+	private enum Direction{up, down, left, right};
+	private Direction direction;
+
+	public float speed;
+	public float upperY;
+	public float lowerY;
+	public bool flagUpDown;
+	public float lefterX;
+	public float righterX;
+
+	// Use this for initialization
+	void Start () {
+		if(flagUpDown)
+			direction = Direction.up;
+		if(!flagUpDown)
+			direction = Direction.right;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch(direction){
+			case Direction.up:
+				transform.position += Vector3.up * speed * Time.deltaTime;
+				break;
+			case Direction.down:
+				transform.position += Vector3.down * speed * Time.deltaTime;
+				break;
+			case Direction.left:
+				transform.position += Vector3.left * speed * Time.deltaTime;
+				break;
+			case Direction.right:
+				transform.position += Vector3.right * speed * Time.deltaTime;
+				break;
+		}
+
+		if(flagUpDown){
+			if(transform.position.y > upperY)
+				direction = Direction.down;
+			if(transform.position.y < lowerY)
+				direction = Direction.up;
+		}
+		if(!flagUpDown){
+			if(transform.position.x < lefterX)
+				direction = Direction.right;
+			if(transform.position.x > righterX)
+				direction = Direction.left;
+		}
+		
+	}
+}
